1cf1125152e8ab4dc3ae0e1b1579e10619438021
xiaoni0v0 <nyj2007@163.com>
2024-07-14 00:54:40 +0800

    refactor: 设置自动保存，并删除“保存设置”的按钮

--- cyber_life/gui/settings_dialog.py
+++ cyber_life/gui/settings_dialog.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QCloseEvent, QKeyEvent
-from PyQt5.QtWidgets import QDialog, QCheckBox, QSlider, QPushButton, QMessageBox, QLabel
+from PyQt5.QtWidgets import QDialog, QCheckBox, QSlider, QPushButton, QLabel
 
 from cyber_life.gui.about_dialog import AboutDialog
 from cyber_life.service.settings import SETTINGS
@@ -21,50 +21,47 @@ class SettingsDialog(QDialog):
         # 设置icon
         self.setWindowIcon(QIcon(":/icon.ico"))
 
-        # 小鱼
+        # 显示小鱼
         self.check_box_fish = QCheckBox("显示小鱼", self)
-        self.check_box_fish.move(10, 10)
+        self.check_box_fish.move(10, 10)  # 放置在窗口里
         self.check_box_fish.setChecked(SETTINGS.is_fish_visible)  # 初始状态
-        self.check_box_fish.stateChanged.connect(self.change_settings_display_fish)  # 状态改变时的槽函数
+        self.check_box_fish.setToolTip("不显示鱼≠没有鱼，资源正常消耗")  # 提示信息
+        self.check_box_fish.stateChanged.connect(self.change_display_fish)  # 状态改变时的槽函数
         # 显示小鱼信息
         self.check_box_fish_info = QCheckBox("显示小鱼信息", self)
         self.check_box_fish_info.move(200, 10)
         self.check_box_fish_info.setChecked(SETTINGS.is_fish_info_visible)
-        self.check_box_fish_info.stateChanged.connect(self.change_settings_display_fish_info)
         self.check_box_fish_info.setEnabled(SETTINGS.is_fish_visible)
+        self.check_box_fish_info.stateChanged.connect(self.change_display_fish_info)
 
-        # 内存
+        # 自定义交换内存高度
         self.check_box_memory = QCheckBox("自定义交换内存高度", self)
         self.check_box_memory.move(10, 40)
         self.check_box_memory.setChecked(SETTINGS.is_swap_memory_fixed)
-        self.check_box_memory.stateChanged.connect(self.change_settings_display_memory)
-        # 一个滑动条组件，从0到100，可以设置交换内存的高度比例
+        self.check_box_memory.stateChanged.connect(self.change_custom_memory)
+        # 一个滑动条组件，从 0 到 99，可以设置交换内存的高度比例
         self.slider_memory = QSlider(Qt.Horizontal, self)
         self.slider_memory.setGeometry(10, 70, 380, 30)
         self.slider_memory.setMinimum(0)
         self.slider_memory.setMaximum(99)  # 最大值不要调100，顶部剩余距离为0导致程序崩溃
         self.slider_memory.setValue(int(SETTINGS.swap_memory_height_rate * 100))
-        self.slider_memory.valueChanged.connect(self.change_memory_height)
         self.slider_memory.setEnabled(SETTINGS.is_swap_memory_fixed)
+        self.slider_memory.valueChanged.connect(self.change_custom_memory_height)
 
         # 投喂食物概率
-        self.put_food_rate_label = QLabel("投喂食物概率：", self)
-        self.put_food_rate_label.move(10, 120)
+        self.label_put_food_rate = QLabel("投喂食物概率：", self)
+        self.label_put_food_rate.move(10, 120)
         # 一个滑动调组件，从0到100，设置投喂食物的概率
         self.slider_feed = QSlider(Qt.Horizontal, self)
         self.slider_feed.setGeometry(10, 150, 380, 30)
         self.slider_feed.setMinimum(0)
         self.slider_feed.setMaximum(100)
         self.slider_feed.setValue(int(SETTINGS.put_food_rate * 100))
-        self.slider_feed.valueChanged.connect(self.change_feed_rate)
+        self.slider_feed.valueChanged.connect(self.change_feed_probability)
 
-        # 底部的保存设置按钮
-        self.button_save = QPushButton("保存设置", self)
-        self.button_save.move(10, 200)
-        self.button_save.clicked.connect(self.save_settings)
         # 底部右下角的关于按钮
         self.button_about = QPushButton("关于", self)
-        self.button_about.move(200, 200)
+        self.button_about.move(10, 200)
         self.button_about.clicked.connect(self.show_about)
 
         # 同 main.py 里 MainWindow.__init__() 里对 dialog 的处理，不再赘述
@@ -80,53 +77,47 @@ class SettingsDialog(QDialog):
         SETTINGS.save_to_json()
 
         # 弹出保存成功的消息框
-        msg_box = QMessageBox()
-        msg_box.setWindowTitle("保存成功")
-        msg_box.setWindowIcon(QIcon(":/icon.ico"))
-        msg_box.setText("设置已成功保存。")
-        msg_box.setIcon(QMessageBox.Information)
-        msg_box.setStandardButtons(QMessageBox.Ok)
-        msg_box.exec_()
+        # msg_box = QMessageBox()
+        # msg_box.setWindowTitle("保存成功")
+        # msg_box.setWindowIcon(QIcon(":/icon.ico"))
+        # msg_box.setText("设置已成功保存。")
+        # msg_box.setIcon(QMessageBox.Information)
+        # msg_box.setStandardButtons(QMessageBox.Ok)
+        # msg_box.exec_()
 
-    def change_settings_display_fish(self, state):
+    def change_display_fish(self, state):
         """
         改变是否显示小鱼
         """
-        if state == Qt.Checked:
-            self.check_box_fish.setChecked(True)
-            SETTINGS.is_fish_visible = True
-            self.check_box_fish_info.setEnabled(True)  # 显示小鱼时，显示小鱼信息也可用
-        else:
-            self.check_box_fish.setChecked(False)
-            SETTINGS.is_fish_visible = False
-            self.check_box_fish_info.setEnabled(False)  # 不显示小鱼时，禁用显示小鱼信息
+        is_button_checked = state == Qt.Checked
 
-    def change_settings_display_fish_info(self, state):
+        SETTINGS.is_fish_visible = is_button_checked
+        self.check_box_fish_info.setEnabled(is_button_checked)  # 显示小鱼时，显示小鱼信息也可用
+
+        self.save_settings()
+
+    def change_display_fish_info(self, state):
         """
         改变是否显示小鱼 *信息*
         """
-        if state == Qt.Checked:
-            self.check_box_fish_info.setChecked(True)
-            SETTINGS.is_fish_info_visible = True
-        else:
-            self.check_box_fish_info.setChecked(False)
-            SETTINGS.is_fish_info_visible = False
+        is_button_checked = state == Qt.Checked
 
-    def change_settings_display_memory(self, state):
-        """
-        改变是否自定义交换内存高度
-        """
-        if state == Qt.Checked:
-            self.check_box_memory.setChecked(True)
-            SETTINGS.is_swap_memory_fixed = True
-            self.slider_memory.setEnabled(True)  # 自定义交换内存高度时，滑动条可用
-        else:
-            self.check_box_memory.setChecked(False)
-            SETTINGS.is_swap_memory_fixed = False
-            self.slider_memory.setEnabled(False)  # 不自定义交换内存高度时，禁用滑动条
+        SETTINGS.is_fish_info_visible = is_button_checked
 
-    @staticmethod
-    def change_memory_height(value):
+        self.save_settings()
+
+    def change_custom_memory(self, state):
+        """
+        改变是否自定义交换内存
+        """
+        is_button_checked = state == Qt.Checked
+
+        SETTINGS.is_swap_memory_fixed = is_button_checked
+        self.slider_memory.setEnabled(is_button_checked)  # 自定义交换内存高度时，滑动条可用
+
+        self.save_settings()
+
+    def change_custom_memory_height(self, value):
         """
         改变交换内存的高度比例
         """
@@ -134,10 +125,15 @@ class SettingsDialog(QDialog):
                 value / 100
         )  # 写入SETTINGS，在LIFE_TANK中判断，如果不将交换内存固定，则生效
 
-    # 滑动条的槽函数
-    @staticmethod
-    def change_feed_rate(value: int):
+        self.save_settings()
+
+    def change_feed_probability(self, value: int):
+        """
+        改变投喂食物的概率
+        """
         SETTINGS.put_food_rate = value / 100
+
+        self.save_settings()
 
     def closeEvent(self, event: QCloseEvent):
         """
